b67dad23ac7873f7b4bab6f1a2c71eb05607738a
Alan Schelten <a.schelten@gmail.com>
2016-03-06 20:34:35 +0100

    Evaluate working, copy and open interpreter not.

---
+++ execute.py
@@ -0,0 +1,43 @@
+import sys
+from workflow import Workflow
+from cStringIO import StringIO
+from math import *
+from random import *
+from re import *
+from calendar import *
+
+import contextlib
+@contextlib.contextmanager
+def capture():
+    import sys
+    from cStringIO import StringIO
+    oldout,olderr = sys.stdout, sys.stderr
+    try:
+        out=[StringIO(), StringIO()]
+        sys.stdout,sys.stderr = out
+        yield out
+    finally:
+        sys.stdout,sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
+
+def main(wf):
+    args = wf.args
+    command = ' '.join(args)
+    answer = None
+    with capture() as out:
+        try:
+            answer = eval(command)
+        except SyntaxError:
+            try:
+                exec(command)
+            except Exception as e:
+                answer = str(e)
+    if not answer:
+        answer = out[0]
+    wf.add_item(title=str(answer), subtitle=command, valid=True)
+    wf.send_feedback()
+
+if __name__ == "__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
